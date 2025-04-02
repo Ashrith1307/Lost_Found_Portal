6e237d3b9b67515925ce7052cfd025e5115315d1
Ashrith1307 <devarapallyashrithreddy@gmail.com>
2025-04-03 00:33:56 +0530

    Building a Frontend

--- Frontend/my-app/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import ButtonComponent from "./Components/ButtonComponent";
-import Reject from "./Components/ButtonComponent";
-
-export default function Even() {
-  const bool = false;
-  return(
-    <h1>hello world!!!</h1>
-  );
-}
