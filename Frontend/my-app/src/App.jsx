03bb7066de63d245b981b871da6e7af0831ccd58
SarikaSrujansai <24h51a67b9@cmrcet.ac.in>
2025-04-02 10:11:47 +0530

    First Commit

--- Frontend/my-app/src/App.jsx
+++ Frontend/my-app/src/App.jsx
@@ -3,5 +3,7 @@ import Reject from "./Components/ButtonComponent";
 
 export default function Even() {
   const bool = false;
-  return <>{bool ? <ButtonComponent /> : <Reject />}</>;
+  return(
+    <h1>hello world!!!</h1>
+  );
 }
